dcfbd59b492c012a0b4edef3a369c78c86022cbd
unknown <masrizaleka@umsida.ac.id>
2021-01-07 16:07:06 +0700

    Laporan penjualan fix

--- application/databases/mutiaract_cos.sql
+++ application/databases/mutiaract_cos.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 17, 2020 at 03:11 AM
--- Server version: 10.3.27-MariaDB
--- PHP Version: 7.3.6
+-- Generation Time: Jan 06, 2021 at 04:23 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -38,6 +37,15 @@ CREATE TABLE `barang` (
   `HARGA_BELI_BARANG` int(11) DEFAULT NULL,
   `HARGA_JUAL_BARANG` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`ID_BARANG`, `ID_PENGGUNA`, `ID_SUPPLIER`, `ID_SATUAN`, `NAMA_BARANG`, `STOK_BARANG`, `HARGA_BELI_BARANG`, `HARGA_JUAL_BARANG`) VALUES
+(1, 4, 1, 1, 'gantungan biru', 21, 500000, 510000),
+(2, 4, 1, 1, 'Sabun', 12, 1200, 12000),
+(3, 4, 1, 1, 'tags putih', 11, 1340, 2000);
 
 -- --------------------------------------------------------
 
@@ -144,9 +152,9 @@ CREATE TABLE `keys` (
   `user_id` int(11) NOT NULL,
   `key` varchar(40) NOT NULL,
   `level` int(2) NOT NULL,
-  `ignore_limits` tinyint(1) NOT NULL DEFAULT 0,
-  `is_private_key` tinyint(1) NOT NULL DEFAULT 0,
-  `ip_addresses` text DEFAULT NULL,
+  `ignore_limits` tinyint(1) NOT NULL DEFAULT '0',
+  `is_private_key` tinyint(1) NOT NULL DEFAULT '0',
+  `ip_addresses` text,
   `date_created` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -315,7 +323,7 @@ INSERT INTO `pengguna` (`ID_PENGGUNA`, `ID_HAK_AKSES`, `NAMA_PENGGUNA`, `EMAIL_P
 (4, 3, 'Gudang MCT', 'gudang@gmail.com', '043552900_1572858828-71890045_694906974353654_3305990052531100815_n.jpg', '$2y$10$dT/Yf9OD3Lg1eI2AshUOhu0OmU6K2bJiwuRgbBIIsI5s5oJ4sUbZe', 1, '2020-10-05'),
 (5, 4, 'Dewi', 'sales@gmail.com', '1.jpg', '$2y$10$NevsXUioV59InL.j5xxVduewl7rSlNVRudB5Ccni1yxB6Mhfw8ajy', 1, '2020-10-05'),
 (7, 4, 'Dian', 'sales2@gmail.com', '49966-artis-tik-tok-nadira-zerlinda-suaracomismail.jpg', '$2y$10$NevsXUioV59InL.j5xxVduewl7rSlNVRudB5Ccni1yxB6Mhfw8ajy', 1, '2020-10-23'),
-(9, 4, 'MASRIZAL EKA YULIANTO', 'masrizal04@gmail.com', 'default.jpg', '$2y$10$NBGFaLn5WbT/FXXKr7l57OuedYHVXpW1FY0179V46svFE3MllXeUu', 1, '2020-12-16'),
+(9, 4, 'masrizal', 'masrizal04@gmail.com', 'default.jpg', '$2y$10$NBGFaLn5WbT/FXXKr7l57OuedYHVXpW1FY0179V46svFE3MllXeUu', 1, '2020-12-16'),
 (10, 4, 'pijar dwi kusima', 'pijar@gmail.com', 'default.jpg', '$2y$10$UVGU9i0hSb/df1OhGHTuNuxzJwz6FYxDh/DVi4RcsYckJ3iNpbksW', 1, '2020-12-16');
 
 -- --------------------------------------------------------
@@ -485,7 +493,7 @@ CREATE TABLE `surat_jalan` (
   `ID_SURAT_JALAN` int(11) NOT NULL,
   `ID_PENGGUNA` int(11) DEFAULT NULL,
   `NO_SURAT_JALAN` varchar(50) NOT NULL,
-  `STATUS_SURAT_JALAN` int(1) NOT NULL DEFAULT 0,
+  `STATUS_SURAT_JALAN` int(1) NOT NULL DEFAULT '0',
   `TGL_SURAT_JALAN` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -639,7 +647,7 @@ ALTER TABLE `surat_jalan`
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `ID_BARANG` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID_BARANG` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `detail_pesan_ulang`
