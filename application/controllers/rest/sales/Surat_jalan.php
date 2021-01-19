57a0aa9bbaad909c0773faa28dd48b94d7b81a70
unknown <masrizaleka@umsida.ac.id>
2021-01-19 10:35:24 +0700

    tambah detail PU dan detail surat jalan fix

--- application/controllers/rest/sales/Surat_jalan.php
+++ application/controllers/rest/sales/Surat_jalan.php
@@ -31,6 +31,33 @@ class Surat_jalan extends CI_Controller
         $json = json_encode($respon);
         echo $json;
     }
+
+    public function detail_surat_jalan()
+    {
+        $id_surat_jalan = htmlspecialchars($this->input->post('id_surat_jalan'), true);
+        $api_key = htmlspecialchars($this->input->post('API-KEY'), true);
+
+        $cek_api_key = $this->api_m->CekApiKey($api_key);
+        if ($cek_api_key->num_rows() > 0) {
+
+            $detail_surat_jalan = $this->surat_jalan_m->SuratJalanById($id_surat_jalan);
+            $detail_barang_surat_jalan = $this->surat_jalan_m->detailBarangSuratJalan($id_surat_jalan);
+            $respon = [
+                'status' => true,
+                'message' => "Data berhasil didapatkan",
+                'detail_surat_jalan' => $detail_surat_jalan->row(),
+                'detail_barang_surat_jalan' => $detail_barang_surat_jalan->result(),
+            ];
+
+        } else {
+            $respon = [
+                'status' => false,
+                'message' => "Error API Key"
+            ];
+        }
+        $json = json_encode($respon);
+        echo $json;
+    }
 }
 
 /* End of file Surat_jala.php */
--- application/models/api/sales/Surat_jalan_m.php
+++ application/models/api/sales/Surat_jalan_m.php
@@ -13,6 +13,22 @@ class Surat_jalan_m extends CI_Model
         $query = $this->db->get();
         return $query;
     }
+
+    public function SuratJalanById($id_surat_jalan)
+    {
+        return $this->db->get_where('surat_jalan', ['ID_SURAT_JALAN' => $id_surat_jalan]);
+    }
+
+    public function detailBarangSuratJalan($id_surat_jalan)
+    {
+        return $this->db->query(
+                "
+                SELECT * FROM detail_surat_jalan dsj 
+                JOIN barang b ON b.ID_BARANG = dsj.ID_BARANG
+                WHERE dsj.ID_SURAT_JALAN = '$id_surat_jalan'
+                "
+        );
+    }
 }
 
 /* End of file Surat_jalan_m.php */
